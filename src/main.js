3e749abaa396bd19956ff1c0a86e26a6f5ed783f
Daniel <daniel_vito@yahoo.com.br>
2022-12-20 22:21:52 -0300

    Specific folder and file for routing

--- src/main.js
+++ src/main.js
@@ -1,23 +1,5 @@
 import { createApp } from "vue";
-import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
-import ViewNotes from "@/views/ViewNotes.vue";
-import ViewStats from "@/views/ViewStats.vue";
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "notes",
-      component: ViewNotes,
-    },
-    {
-      path: "/stats",
-      name: "stats",
-      component: ViewStats,
-    },
-  ],
-});
+import router from "@/router";
 
 createApp(App).use(router).mount("#app");
