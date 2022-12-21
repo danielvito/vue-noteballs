aeae8afcaf0a16fb578a8553e1f2c39f9f0483cd
Daniel <daniel_vito@yahoo.com.br>
2022-12-21 16:28:24 -0300

    Notes mock implementation

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,24 +3,32 @@
     <div class="card has-background-link-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea
+            class="textarea"
+            v-model="newNote"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link is-light">Add New Note</button>
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link is-light"
+          >
+            Add New Note
+          </button>
         </div>
       </div>
     </div>
 
-    <div v-for="i in 3" class="card mb-4">
+    <div v-for="note in notes" :key="note.id" class="card mb-4">
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-          doloribus sunt non suscipit eos debitis sequi tempore facilis, quas
-          aspernatur asperiores, excepturi sit? Suscipit excepturi nobis
-          dignissimos assumenda, officia animi!
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
@@ -31,8 +39,36 @@
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+
+const newNote = ref("");
+const newNoteRef = ref(null);
+
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate doloribus sunt non suscipit eos debitis sequi tempore facilis, quas aspernatur asperiores, excepturi sit? Suscipit excepturi nobis dignissimos assumenda, officia animi!",
+  },
+  {
+    id: "id2",
+    content: "Shor text",
+  },
+]);
+
+const addNote = () => {
+  let currentDate = new Date().getTime(),
+    id = currentDate.toString();
+
+  let note = { id, content: newNote.value };
+
+  notes.value.unshift(note);
+  newNote.value = "";
+  newNoteRef.value.focus();
+
+  // console.log(note);
+};
 </script>
 
 <style></style>
