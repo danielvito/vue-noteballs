1e6fb3024583f6fb9a1bbb712c10bf749397bb51
Daniel <daniel_vito@yahoo.com.br>
2022-12-22 14:51:28 -0300

    Added autofocus directive

--- src/components/notes/MergeNote.vue
+++ src/components/notes/MergeNote.vue
@@ -12,6 +12,7 @@
           class="textarea"
           :placeholder="placeholder"
           ref="textAreaRef"
+          v-autofocus
         />
       </div>
     </div>
@@ -26,6 +27,7 @@
 
 <script setup>
 import { ref } from "vue";
+import { vAutofocus } from "@/directives/vAutofocus";
 
 const props = defineProps({
   modelValue: {
--- src/views/ViewStats.vue
+++ src/views/ViewStats.vue
@@ -18,11 +18,18 @@
         </tr>
       </tbody>
     </table>
+    <input
+      type="text"
+      class="input"
+      placeholder="Do you love noteballs?"
+      v-autofocus
+    />
   </div>
 </template>
 
 <script setup>
 import { useStoreNotes } from "@/stores/storeNotes";
+import { vAutofocus } from "@/directives/vAutofocus";
 
 const storeNotes = useStoreNotes();
 </script>
